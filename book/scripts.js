e5599957b75dddb304f3fc891244e5d8cf13c081
Owen Prosser <owenprosser@users.noreply.github.com>
2025-03-02 16:02:04 +0000

    Added 404 book id

--- book/scripts.js
+++ book/scripts.js
@@ -8,20 +8,34 @@ window.addEventListener('load', function() {
 
 function reloadContent(jsonData)
 {
+    var bookCount = Object.keys(jsonData.books).length;
+
+    var bookIndex = window.location.hash.substr(1);
+    bookIndex -= 1;
+
     var img = document.getElementById("image");
     var author = document.getElementById("author");
     var title = document.getElementById("title");
     var synopsis = document.getElementById("synopsis");
     var selector = document.getElementById("selector");
-    var date = document.getElementById("date");
+    var date = document.getElementById("date");  
 
-    var bookIndex = window.location.hash.substr(1);
-    bookIndex -= 1;
-
-    img.src =  "../" + jsonData.books[bookIndex].image_url;
-    author.textContent = jsonData.books[bookIndex].author;
-    title.textContent = jsonData.books[bookIndex].title;
-    synopsis.textContent = jsonData.books[bookIndex].synopsis;
-    selector.textContent = jsonData.books[bookIndex].selector;
-    date.textContent = jsonData.books[bookIndex].date;
+    if(bookIndex < bookCount)
+    {
+        img.src =  "../" + jsonData.books[bookIndex].image_url;
+        author.textContent = jsonData.books[bookIndex].author;
+        title.textContent = jsonData.books[bookIndex].title;
+        synopsis.textContent = jsonData.books[bookIndex].synopsis;
+        selector.textContent = jsonData.books[bookIndex].selector;
+        date.textContent = jsonData.books[bookIndex].date;
+    }
+    else
+    {
+        img.src =  "../" + "covers/404.jpg";
+        author.textContent = "Book not found";
+        title.textContent = "index out of range";
+        synopsis.textContent = "Check URL ID";
+        selector.textContent = "";
+        date.textContent = "";
+    }
 }
